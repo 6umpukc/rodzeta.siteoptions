add42d10ffeeb2f0f509ab07c8c887868e7a4aaf
Semyonov Roman <semyonov.roman@citrus-soft.ru>
2024-04-26 17:44:55 +0300

    fix

---
+++ lib/Action/Help.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Rodzeta\Siteoptions\Action;
+
+final class Help extends Base
+{
+	public function getDescription()
+	{
+		return 'bx help - Выводит информацию по поддерживаемым командам';
+	}
+
+	public function run()
+	{
+		//...
+	}
+}
+
